2bb7c74bbde608d6d931383d7b5a6dfc5195efa7
TamalSana <tamalsana23@gmail.com>
2026-09-14 13:41:42 +0530

    divs created using js-divs created using flexbox

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container');
+
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement('div');
+  div.classList.add('square');
+  container.appendChild(div);
+}
+
+
